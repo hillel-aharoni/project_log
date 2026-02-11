826a88f831eb90ab1f98448fa70f9d5f3497d7f7
hillel aharoni <hillel2552022@gmail.com>
2026-02-11 09:30:20 +0200

    Update all files following step 2

--- reader.py
+++ reader.py
@@ -6,7 +6,6 @@ def load_csv(file_path):
     file_path (str): נתיב לקובץ ה‑CSV
 
     מחזיר:
-    list: רשימת רשימות, כאשר כל רשימה פנימית מייצגת שורה עם השדות:
     [timestamp, source_ip, dest_ip, port, protocol, size]
     """
 
@@ -14,7 +13,7 @@ def load_csv(file_path):
     with open(file_path, 'r') as file:
         for line in file:
             line = line.strip()
-            if line:  # דלג על שורות ריקות
+            if line:
                 # פצל לפי פסיק והסר רווחים מיותרים מכל שדה
                 row = [field.strip() for field in line.split(',')]
                 data.append(row)
